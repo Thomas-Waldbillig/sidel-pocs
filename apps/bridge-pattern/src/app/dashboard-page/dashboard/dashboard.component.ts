b36e320861f229eae231395bcf6c2c4ad4b19cbe
Thomas Waldbillig <waldbillig.thomas@gmail.com>
2023-09-05 15:59:05 +0200

    feat: moving widget creation to dashboard component

--- apps/bridge-pattern/src/app/dashboard-page/dashboard/dashboard.component.ts
+++ apps/bridge-pattern/src/app/dashboard-page/dashboard/dashboard.component.ts
@@ -1,25 +1,81 @@
 import { CommonModule } from '@angular/common';
-import { ChangeDetectionStrategy, Component } from '@angular/core';
+import {
+  AfterContentInit,
+  AfterViewInit,
+  ChangeDetectionStrategy,
+  ChangeDetectorRef,
+  Component,
+  QueryList,
+  ViewChildren,
+  ViewContainerRef,
+  inject,
+} from '@angular/core';
 import { Widget } from '../../models';
 import { WidgetWrapperComponent } from '../widget-wrapper/widget-wrapper.component';
+import {
+  WidgetFourComponent,
+  WidgetOneComponent,
+  WidgetThreeComponent,
+  WidgetTwoComponent,
+} from '../widgets';
 import { widgetData } from './dashboard.data';
 
 @Component({
   selector: 'sp-dashboard',
   standalone: true,
-  imports: [CommonModule, WidgetWrapperComponent],
+  imports: [CommonModule, WidgetWrapperComponent, WidgetOneComponent],
   template: `
     <h2>Dashboard Component</h2>
 
     <div class="widgets">
       <ng-container *ngFor="let widget of widgets">
-        <sp-widget-wrapper class="widget" [widget]="widget" />
+        <sp-widget-wrapper class="widget" [widget]="widget">
+          <ng-template #widgetWrappers />
+        </sp-widget-wrapper>
       </ng-container>
     </div>
   `,
   styleUrls: ['./dashboard.component.scss'],
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class DashboardComponent {
+export class DashboardComponent implements AfterViewInit, AfterContentInit {
+  private readonly changeDetectorRef = inject(ChangeDetectorRef);
+
   protected widgets: Widget[] = widgetData;
+
+  @ViewChildren('widgetWrappers', { read: ViewContainerRef })
+  private widgetWrappers!: QueryList<ViewContainerRef>;
+
+  public ngAfterContentInit(): void {
+    console.log(this.widgetWrappers);
+  }
+
+  public ngAfterViewInit(): void {
+    this.widgetWrappers.forEach(
+      (ref: ViewContainerRef, index: number): void => {
+        const widgetDatum: Widget = widgetData[index];
+        const component = ref.createComponent(
+          this.getComponentClass(widgetDatum)
+        );
+        component.instance.data = widgetDatum.data;
+      }
+    );
+  }
+
+  // OTHER
+
+  private getComponentClass({ type }: Widget) {
+    switch (type) {
+      case 'widget-one':
+        return WidgetOneComponent;
+      case 'widget-two':
+        return WidgetTwoComponent;
+      case 'widget-three':
+        return WidgetThreeComponent;
+      case 'widget-four':
+        return WidgetFourComponent;
+      default:
+        throw new Error('Invalid widget type');
+    }
+  }
 }
--- apps/bridge-pattern/src/app/dashboard-page/widget-wrapper/widget-wrapper.component.ts
+++ apps/bridge-pattern/src/app/dashboard-page/widget-wrapper/widget-wrapper.component.ts
@@ -2,27 +2,24 @@ import { CommonModule } from '@angular/common';
 import {
   ChangeDetectionStrategy,
   Component,
+  ContentChild,
+  ElementRef,
   HostBinding,
   Input,
-  OnInit,
-  ViewChild,
-  ViewContainerRef,
 } from '@angular/core';
 import { MatButtonModule } from '@angular/material/button';
 import { MatCardModule } from '@angular/material/card';
 import { Widget } from '../../models';
-import {
-  WidgetFourComponent,
-  WidgetOneComponent,
-  WidgetThreeComponent,
-  WidgetTwoComponent,
-} from '../widgets';
+import { RESIZABLE_WIDGET } from '../behaviors';
+import { WidgetOneComponent } from '../widgets';
 
 @Component({
   selector: 'sp-widget-wrapper',
   standalone: true,
-  imports: [CommonModule, MatCardModule, MatButtonModule],
+  imports: [WidgetOneComponent, CommonModule, MatCardModule, MatButtonModule],
   template: `
+    <div *ngIf="resizableWidget">asd</div>
+
     <mat-card>
       <mat-card-header>
         <mat-card-title>{{ widget.label }}</mat-card-title>
@@ -30,7 +27,7 @@ import {
       </mat-card-header>
 
       <mat-card-content>
-        <ng-template #widgetContainer />
+        <ng-content />
       </mat-card-content>
 
       <mat-card-actions align="end">
@@ -41,7 +38,7 @@ import {
   styleUrls: ['./widget-wrapper.component.scss'],
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class WidgetWrapperComponent implements OnInit {
+export class WidgetWrapperComponent {
   // DEPENDENCY INJECTIONS
 
   // ...
@@ -61,33 +58,11 @@ export class WidgetWrapperComponent implements OnInit {
   @Input({ required: true })
   public widget!: Widget;
 
-  @ViewChild('widgetContainer', { static: true, read: ViewContainerRef })
-  private widgetContainerRef!: ViewContainerRef;
+  // @ContentChild(WidgetOneComponent)
+  @ContentChild(RESIZABLE_WIDGET)
+  public resizableWidget!: ElementRef<HTMLElement>;
 
   // LIFECYCLE HOOKS
 
-  public ngOnInit(): void {
-    // TODO: move to a service
-    this.widgetContainerRef.clear();
-    const component = this.widgetContainerRef.createComponent(
-      this.getComponentClass(this.widget)
-    );
-    component.instance.data = this.widget.data;
-  }
-
-  // OTHER
-  private getComponentClass({ type }: Widget) {
-    switch (type) {
-      case 'widget-one':
-        return WidgetOneComponent;
-      case 'widget-two':
-        return WidgetTwoComponent;
-      case 'widget-three':
-        return WidgetThreeComponent;
-      case 'widget-four':
-        return WidgetFourComponent;
-      default:
-        throw new Error('Invalid widget type');
-    }
-  }
+  // ...
 }
