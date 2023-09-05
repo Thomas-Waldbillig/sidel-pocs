2a879671b5348a124ffc3c86239ab6df7c3fd861
Thomas Waldbillig <waldbillig.thomas@gmail.com>
2023-09-05 13:27:33 +0200

    feat: implementing dynamic widget loading

--- apps/bridge-pattern/src/app/dashboard-page/widget-wrapper/widget-wrapper.component.ts
+++ apps/bridge-pattern/src/app/dashboard-page/widget-wrapper/widget-wrapper.component.ts
@@ -4,10 +4,17 @@ import {
   Component,
   HostBinding,
   Input,
+  OnInit,
+  ViewChild,
+  ViewContainerRef,
 } from '@angular/core';
 import { MatButtonModule } from '@angular/material/button';
 import { MatCardModule } from '@angular/material/card';
 import { Widget } from '../../models';
+import { WidgetFourComponent } from '../widget-four/widget-four.component';
+import { WidgetOneComponent } from '../widget-one/widget-one.component';
+import { WidgetThreeComponent } from '../widget-three/widget-three.component';
+import { WidgetTwoComponent } from '../widget-two/widget-two.component';
 
 @Component({
   selector: 'sp-widget-wrapper',
@@ -21,7 +28,7 @@ import { Widget } from '../../models';
       </mat-card-header>
 
       <mat-card-content>
-        <pre>{{ widget.data | json }}</pre>
+        <ng-template #widgetContainer />
       </mat-card-content>
 
       <mat-card-actions align="end">
@@ -32,9 +39,12 @@ import { Widget } from '../../models';
   styleUrls: ['./widget-wrapper.component.scss'],
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class WidgetWrapperComponent {
-  @Input({ required: true })
-  public widget!: Widget;
+export class WidgetWrapperComponent implements OnInit {
+  // DEPENDENCY INJECTIONS
+
+  // ...
+
+  // VARIABLES
 
   @HostBinding('style.--height-span')
   protected get heightSpan(): number {
@@ -44,5 +54,34 @@ export class WidgetWrapperComponent {
   @HostBinding('style.--width-span')
   protected get widthSpan(): number {
     return this.widget.position.width;
+  }
+
+  @Input({ required: true })
+  public widget!: Widget;
+
+  @ViewChild('widgetContainer', { static: true, read: ViewContainerRef })
+  private widgetContainerRef!: ViewContainerRef;
+
+  // LIFECYCLE HOOKS
+
+  public ngOnInit(): void {
+    // TODO: move to a service
+    this.widgetContainerRef.clear();
+    switch (this.widget.type) {
+      case 'widget-one':
+        this.widgetContainerRef.createComponent(WidgetOneComponent);
+        break;
+      case 'widget-two':
+        this.widgetContainerRef.createComponent(WidgetTwoComponent);
+        break;
+      case 'widget-three':
+        this.widgetContainerRef.createComponent(WidgetThreeComponent);
+        break;
+      case 'widget-four':
+        this.widgetContainerRef.createComponent(WidgetFourComponent);
+        break;
+      default:
+        throw new Error('Invalid widget type');
+    }
   }
 }
