97d076841baeadaa64283174c9fbcfec1bfddcf3
Thomas Waldbillig <waldbillig.thomas@gmail.com>
2023-09-05 11:03:32 +0200

    chore: renaming app

--- apps/bridge-pattern/src/app/app.component.ts
+++ apps/bridge-pattern/src/app/app.component.ts
@@ -1,14 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterModule } from '@angular/router';
-import { NxWelcomeComponent } from './nx-welcome.component';
 
 @Component({
   standalone: true,
-  imports: [NxWelcomeComponent, RouterModule],
+  imports: [RouterModule],
   selector: 'sidel-pocs-root',
-  templateUrl: './app.component.html',
+  template: `<h1>Welcome bridge-pattern</h1>
+    <router-outlet></router-outlet>`,
   styleUrls: ['./app.component.scss'],
 })
-export class AppComponent {
-  title = 'bridge';
-}
+export class AppComponent {}
