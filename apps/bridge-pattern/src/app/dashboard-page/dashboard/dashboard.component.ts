1af03e950b193189ff77f14dd671b586a9afe452
Thomas Waldbillig <waldbillig.thomas@gmail.com>
2023-09-05 15:56:23 +0200

    feat: moving widget data to separate file

--- apps/bridge-pattern/src/app/dashboard-page/dashboard/dashboard.component.ts
+++ apps/bridge-pattern/src/app/dashboard-page/dashboard/dashboard.component.ts
@@ -2,6 +2,7 @@ import { CommonModule } from '@angular/common';
 import { ChangeDetectionStrategy, Component } from '@angular/core';
 import { Widget } from '../../models';
 import { WidgetWrapperComponent } from '../widget-wrapper/widget-wrapper.component';
+import { widgetData } from './dashboard.data';
 
 @Component({
   selector: 'sp-dashboard',
@@ -20,78 +21,5 @@ import { WidgetWrapperComponent } from '../widget-wrapper/widget-wrapper.compone
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class DashboardComponent {
-  protected widgets: Widget[] = [
-    {
-      type: 'widget-one',
-      label: 'label 01',
-      data: { value: 1 },
-      position: { height: 1, width: 1 },
-    },
-    {
-      type: 'widget-four',
-      label: 'label 02',
-      data: { value: 2 },
-      position: { height: 1, width: 2 },
-    },
-    {
-      type: 'widget-three',
-      label: 'label 03',
-      data: { value: 3 },
-      position: { height: 1, width: 2 },
-    },
-    {
-      type: 'widget-two',
-      label: 'label 04',
-      data: { value: 4 },
-      position: { height: 1, width: 3 },
-    },
-    {
-      type: 'widget-two',
-      label: 'label 05',
-      data: { value: 5 },
-      position: { height: 2, width: 1 },
-    },
-    {
-      type: 'widget-four',
-      label: 'label 06',
-      data: { value: 6 },
-      position: { height: 2, width: 2 },
-    },
-    {
-      type: 'widget-one',
-      label: 'label 07',
-      data: { value: 7 },
-      position: { height: 2, width: 2 },
-    },
-    {
-      type: 'widget-two',
-      label: 'label 08',
-      data: { value: 8 },
-      position: { height: 2, width: 3 },
-    },
-    {
-      type: 'widget-three',
-      label: 'label 09',
-      data: { value: 9 },
-      position: { height: 3, width: 1 },
-    },
-    {
-      type: 'widget-three',
-      label: 'label 10',
-      data: { value: 10 },
-      position: { height: 3, width: 2 },
-    },
-    {
-      type: 'widget-one',
-      label: 'label 11',
-      data: { value: 11 },
-      position: { height: 3, width: 2 },
-    },
-    {
-      type: 'widget-four',
-      label: 'label 12',
-      data: { value: 12 },
-      position: { height: 3, width: 3 },
-    },
-  ];
+  protected widgets: Widget[] = widgetData;
 }
